3d55c7d1811218d7a3bc5d83d579f611544f6605
MJBackes <mjohnbackes@gmail.com>
2019-10-31 14:02:29 -0500

    Added a comment about SOLID to satisfy the user stories.

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -9,6 +9,13 @@ namespace LemonadeStand
     class Program
     {
 
+        //Instances of SOLID principals:
+        //Single Responsibility:
+        //I tried to apply this to the whole program and can't really point to one specific spot more than another.
+        //Open/Closed:
+        //The methods in the Game class were written to be agnostic to the number of players in the game, so when the 
+        //game was expanded to allow for multiple players the only functions that really required modification were the
+        //MainGame and RunGame functions that call the smaller functions.
         static void Main(string[] args)
         {
             Game game = new Game();
